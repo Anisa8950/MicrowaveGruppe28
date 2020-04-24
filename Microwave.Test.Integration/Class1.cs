51d5300502be4debde0d3796b4b59d5137137b91
Stonani <stefine.varde@gmail.com>
2020-04-24 12:43:10 +0200

    Integrationtest folder with Nunit and NSubstitute

---
+++ Microwave.Test.Integration/Class1.cs
@@ -0,0 +1,22 @@
+﻿using NUnit.Framework;
+using NSubstitute;
+
+
+namespace Microwave.Test.Integration
+{
+    [TestFixture]
+    public class Class1
+    {
+        [SetUp]
+        public void Setup()
+        {
+            
+        }
+
+        [Test]
+        public void Insert_Test_Here()
+        {
+            
+        }
+    }
+}
