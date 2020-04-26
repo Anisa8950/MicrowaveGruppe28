915228815a9208d5018c5858279aa645146c6d11
Stonani <stefine.varde@gmail.com>
2020-04-26 15:02:30 +0200

    IT3 powertube done

--- Microwave.Test.Integration/IT3_PowertubeOutput.cs
+++ Microwave.Test.Integration/IT3_PowertubeOutput.cs
@@ -13,13 +13,14 @@ namespace Microwave.Test.Integration
     {
         private IPowerTube _powertube;
         private IOutput _output;
-        private StringWriter _stringWriter;
+        public StringWriter _stringWriter;
 
         [SetUp]
         public void Setup()
         {
             _output = new Output();
             _powertube = new PowerTube(_output);
+            _stringWriter = new StringWriter();
         }
 
         [TestCase(2)]
@@ -27,41 +28,37 @@ namespace Microwave.Test.Integration
         [TestCase(99)]
         public void TurnOn_Output_PowerTubeWorksWithPower(int power)
         {
-            using (_stringWriter = new StringWriter())
+            using (_stringWriter)
             {
                 Console.SetOut(_stringWriter);
 
                 _powertube.TurnOn(power);
             }
+            Assert.That(_stringWriter.ToString(), Is.EqualTo("PowerTube works with " + power + "\r\n"));
+        }
 
-            Assert.That(_stringWriter.ToString(), Is.EqualTo("PowerTube works with "+power+"\r\n"));
+        [Test]
+        public void TurnOn_TurnOff_Output_PowerTubeTurnedOff()
+        {
+            using (_stringWriter)
+            {
+                Console.SetOut(_stringWriter);
+                _powertube.TurnOn(50);
+                _powertube.TurnOff();
+            }
+            Assert.That(_stringWriter.ToString, Is.EqualTo("PowerTube works with 50\r\nPowerTube turned off\r\n"));
         }
 
         [Test]
         public void TurnOff_Output_Nothing()
         {
-            using (_stringWriter = new StringWriter())
+            using (_stringWriter)
             {
                 Console.SetOut(_stringWriter);
 
                 _powertube.TurnOff();
             }
-
             Assert.That(_stringWriter.ToString(), Is.EqualTo(String.Empty));
-        }
-
-        [Test]
-        public void TurnOff_Output_PowerTubeTurnedOff()
-        {
-            using (_stringWriter = new StringWriter())
-            {
-                Console.SetOut(_stringWriter);
-
-                _powertube.TurnOn(50);
-                _powertube.TurnOff();
-            }
-            
-            Assert.That(_stringWriter.ToString, Is.EqualTo("PowerTube turned off\r\n"));
         }
     }    
 }
