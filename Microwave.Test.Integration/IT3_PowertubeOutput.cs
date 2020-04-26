8234e21df0dc0f4eb09a59ed3317ac9357858587
Stonani <stefine.varde@gmail.com>
2020-04-26 11:47:02 +0200

    MicrowaveOvenreference added IT3 powertube added

---
+++ Microwave.Test.Integration/IT3_PowertubeOutput.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+using NSubstitute;
+using System.IO;
+
+
+namespace Microwave.Test.Integration
+{
+    [TestFixture]
+    public class IT3_PowertubeOutput
+    {
+        private 
+        [SetUp]
+        public void Setup()
+        {
+
+        }
+    }    
+}
