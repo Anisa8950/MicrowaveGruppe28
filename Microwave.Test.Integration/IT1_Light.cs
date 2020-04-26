94a83c34294103a3eb2b92d82840fae337a924bb
Anisa Mohamed Nuur Ali <201806371@post.au.dk>
2020-04-26 21:24:54 +0200

    Added integration step 1

---
+++ Microwave.Test.Integration/IT1_Light.cs
@@ -0,0 +1,30 @@
+﻿using MicrowaveOvenClasses.Boundary;
+using NUnit.Framework;
+using NSubstitute;
+
+
+namespace Microwave.Test.Integration
+{
+    [TestFixture]
+    public class IT1_Light
+    {
+        private Light _light; //skal man erklære dem med deres interfaces eller egetnlige klasser?
+        private Output _output;
+
+        [SetUp]
+        public void Setup()
+        {
+           _output = new Output();
+            _light = new Light(_output);
+            
+        }
+
+        //[Test]
+        //public void TurnOn_ConsoleWritesLightIsTurnedOn()
+        //{
+        //    _light.TurnOn();
+
+            
+        //}
+    }
+}
