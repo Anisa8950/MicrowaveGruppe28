e72e190ef56bc2636d3a5311bd1693a450c1bc1b
Marie-Louise <marie_louise_ryom@hotmail.com>
2020-04-28 12:27:15 +0200

    Tilføjelse af test i IT6

--- Microwave.Test.Integration/IT6_CookControllerTimer.cs
+++ Microwave.Test.Integration/IT6_CookControllerTimer.cs
@@ -16,40 +16,91 @@ namespace Microwave.Test.Integration
     {
         private IDisplay _display;
         private IPowerTube _powerTube;
+        private IUserInterface _userInterface;
         private Output _output;
         private Timer _timer;
         private CookController _cookController;
-        public StringWriter _stringWriter;
+        private StringWriter _stringWriter;
 
         [SetUp]
         public void SetUp()
         {
             _display = Substitute.For<IDisplay>();
             _powerTube = Substitute.For<IPowerTube>();
+            _userInterface = Substitute.For<IUserInterface>();
 
             _output = new Output();
             _timer = new Timer();
-            _cookController = new CookController(_timer,_display,_powerTube);
+            _cookController = new CookController(_timer,_display,_powerTube,_userInterface);
+
+            _stringWriter = new StringWriter();
+            Console.SetOut(_stringWriter);
         }
 
         [Test]
-        public void StartCooking_TimerStart()
-        {
-            
-        }
-
-        [Test]
-        public void StartCooking_TimerTick_ShortEnough()
+        public void StartCooking_TimerTick_WaitForTick_DisplayShowTime()
         {
             ManualResetEvent pause = new ManualResetEvent(false);
 
             _timer.TimerTick += (sender, args) => pause.Set();
+            _cookController.StartCooking(50,60);
+
+            // wait for one tick
+            Assert.That(pause.WaitOne(1100));
+            _display.Received(1).ShowTime(Arg.Any<int>(), Arg.Any<int>());
+        }
+
+        [Test]
+        public void StartCooking_TimerTick_DoNotWaitForTick_DisplayShowsNothing()
+        {
+            ManualResetEvent pause = new ManualResetEvent(false);
+
+            _timer.TimerTick += (sender, args) => pause.Set();
+            _cookController.StartCooking(50,60);
+
+            // wait shorter than a tick
+            Assert.That(!pause.WaitOne(900));
+            _display.DidNotReceive().ShowTime(Arg.Any<int>(), Arg.Any<int>());
+        }
+
+        [Test]
+        public void StartCooking_TimerExpires_WaitForTick_CookingIsDone()
+        {
+            ManualResetEvent pause = new ManualResetEvent(false);
+
+            _timer.Expired += (sender, args) => pause.Set();
             _cookController.StartCooking(50,2);
 
-            // wait for one tick, but no longer
-            Assert.That(pause.WaitOne(1100));
-            _display.Received(1).ShowTime(Arg.Any<int>(),Arg.Any<int>());
-            _display.Received(1).ShowTime(0, 1);
+            // wait for expiration
+            Assert.That(pause.WaitOne(2100));
+            _userInterface.Received(1).CookingIsDone();
         }
+
+        [Test]
+        public void StartCooking_TimerExpires_DoNotWaitForTick_CookingNotDone()
+        {
+            ManualResetEvent pause = new ManualResetEvent(false);
+
+            _timer.Expired += (sender, args) => pause.Set();
+            _cookController.StartCooking(50, 2);
+
+            // wait for expiration
+            Assert.That(!pause.WaitOne(1900));
+            _userInterface.DidNotReceive().CookingIsDone();
+        }
+
+        [Test]
+        public void StopCooking_TimerTick_DoNotWaitForTick_CookingNotDone()
+        {
+            ManualResetEvent pause = new ManualResetEvent(false);
+
+            _timer.Expired += (sender, args) => pause.Set();
+            _cookController.StartCooking(50, 2);
+
+            // wait for expiration
+            Assert.That(!pause.WaitOne(1900));
+            _userInterface.DidNotReceive().CookingIsDone();
+        }
+
     }
 }
